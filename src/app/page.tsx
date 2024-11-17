89123ec875d573e6a74f31e68ac6c61ada7726f1
Shirel Zohar <83185587+Shirel1234@users.noreply.github.com>
2024-11-17 14:58:28 +0200

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Ball from './components/Ball';
 
 const Home: React.FC = () => {
+  console.log("login");
+  
   return (
     <div className="relative w-screen h-screen overflow-hidden bg-gray-100">
       <Ball />
