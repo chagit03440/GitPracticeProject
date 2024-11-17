a7ae158f5990bf9217314f306cf554b7e26f931f
Shirel Zohar <83185587+Shirel1234@users.noreply.github.com>
2024-11-17 15:02:27 +0200

    mtdgfd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Ball from './components/Ball';
 
 const Home: React.FC = () => {
-  console.log("logigfghfhghgjhfn");
+  console.log("logig");
+  console.log("logig");
+
   
   return (
     <div className="relative w-screen h-screen overflow-hidden bg-gray-100">
