c540686e7ba8f3c3af263213012222c3751185dd
chagit03440 <82941665+chagit03440@users.noreply.github.com>
2024-11-17 14:24:29 +0200

    refactor commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noopener dcfghjnoreferrer"
           >
             <Image
               className="dark:invert"
